b8d815ef54e22b04e4ee0ae9e66f021886c44e36
Duy Nguyen <Kduy9180@gmail.com>
2023-11-21 15:48:25 +0700

    fix login signout

---
+++ phone seller/web1-test/end_user/layout_skeletion_js.js
@@ -0,0 +1,44 @@
+/*let key='1';
+
+if(sessionStorage.getItem("key")===null){
+    console.log("key is not initilazed, now key is created");
+    sessionStorage.setItem("key",key);
+}
+else{
+    console.log("use the created key");
+}
+
+key=sessionStorage.getItem("key");
+console.log(key);
+*/
+
+
+function setisLoginTrue(currentTag){
+    sessionStorage.setItem("loginState", "true");
+    console.log("return to index");
+    window.location="index.html"; 
+}
+function setisLoginFalse(currentTag){
+    sessionStorage.setItem("loginState", "false");
+    window.location="index.html"; 
+}
+
+let singout=document.getElementById("singout");
+let login=document.getElementById("login");
+let isLogin=sessionStorage.getItem("loginState");
+console.log("isLogin:"+isLogin);
+if(isLogin===null){
+    isLogin="false";
+    sessionStorage.setItem("loginState", isLogin);
+}
+
+if(isLogin==="true"){
+    console.log("login is hidden");
+    login.style.display="none";
+    singout.style.display="inline";
+}
+else{
+    console.log("signout is hidden");
+    login.style.display="inline";
+    singout.style.display="none";
+}
