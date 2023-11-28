ed06bc6537d3acfe16b765c13789e2f3f613768d
Duy Nguyen <Kduy9180@gmail.com>
2023-11-28 14:32:12 +0700

    a little javascript change to button of search

--- phone seller/web1-test/end_user/layout_skeletion_js.js
+++ phone seller/web1-test/end_user/layout_skeletion_js.js
@@ -51,8 +51,52 @@ let simple_search_form=(simple_search.getElementsByTagName("form"))[0];
 let inputSubmit=(simple_search_form.getElementsByTagName("input"))[1];
 inputSubmit.value="Tìm";
 
-//change advance search image to "tim kiem nang cao"
+//remove advance search image
 let advanced_search=document.getElementById("advanced_search");
 let advanced_search_button=(advanced_search.getElementsByTagName("a"))[0];
 let advanced_search_button_img=(advanced_search_button.getElementsByTagName("img"))[0];
-console.log(advanced_search_button_img);
+/*advanced_search_button_img.setAttribute("src","assets/timkiemnangcao.png");
+advanced_search_button_img.setAttribute("with", "50px");
+advanced_search_button_img.setAttribute("height", "50px");*/
+advanced_search_button_img.remove();
+
+//add "tim kiem nang cao"
+let advanced_search_top=document.createElement("div");
+advanced_search_top.setAttribute("class","menu_top_element");
+advanced_search_top.setAttribute("id","advancedSearch");
+let advanced_search_link=document.createElement("a");
+advanced_search_link.setAttribute("href","advanced_search.html")
+advanced_search_top.appendChild(advanced_search_link);
+advanced_search_link.innerText="Tìm kiếm nâng cao";
+let menu_top=document.getElementById("menu_top");
+menu_top.append(advanced_search_top);
+
+//add onclick action to simple search "tim" button
+inputSubmit.setAttribute("onclick","window.location.replace('search_result.html')");
+simple_search_form.setAttribute("target","_blank");
+
+//change submit to "tim" of advanced search + add onclick action to simple search "tim" button
+let advanced_search_form=document.getElementById("item1");
+let advanced_submit_button=advanced_search_form.lastElementChild;
+advanced_submit_button.value="Tìm";
+advanced_submit_button.setAttribute("onclick","window.location.replace('search_result.html')");
+advanced_search_form.setAttribute("target","_blank");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
