37ff451c223498327eedd09aa524829783dbb0a7
kevin khang <khangau98@gmail.com>
2023-12-04 12:40:35 +0700

    add change attribute form to connect with product found

--- phone seller/web1-test/web_admin/financeStatisticJS.js
+++ phone seller/web1-test/web_admin/financeStatisticJS.js
@@ -13,29 +13,39 @@ function typeOptionChecked(){
     search_product_name.style.display="none";
     search_product_type.style.display="block";
 }
-function SubmitSearch(){
-    let search_result=document.getElementById("search-result");
-    search_result.style.display="block";
-    console.log(search_result);
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const searchByName = document.getElementById("search-product");
+    const searchByType = document.getElementById("search-product-type");
 
-var option=sessionStorage.getItem("option");
-let search_product_name=document.getElementById("search-product");
-let search_product_type=document.getElementById("search-product-type");
-//console.log(search_product_name);
-//console.log(search_product_type);
-if(option===null){
-    option="name";
-    sessionStorage.setItem("option", option);
-}
+    const searchOption = document.getElementsByName("option");
+    for (let i = 0; i < searchOption.length; i++) {
+        searchOption[i].addEventListener('change', function() {
+            if (searchOption[i].checked && searchOption[i].value === 'name') {
+                searchByName.style.display = "block";
+                searchByType.style.display = "none";
+            } else if (searchOption[i].checked && searchOption[i].value === 'type') {
+                searchByType.style.display = "block";
+                searchByName.style.display = "none";
+            }
+        });
+    }
 
-if(option==="name"){
-    search_product_name.style.display="block";
-    search_product_type.style.display="none";
-    console.log(1);
-}
-else{
-    search_product_name.style.display="none";
-    search_product_type.style.display="block";
-    console.log(2);
-}
+    function submitSearch() {
+        const searchResult = document.getElementById("search-result");
+        searchResult.style.display = "block";
+        const loaiSanPham = document.getElementById("loai_san_pham");
+        const loaiValue = document.querySelector('input[name="option"]:checked').value;
+
+        if (loaiValue === "name") {
+            const productName = document.getElementById("product-name").value;
+            loaiSanPham.innerHTML = `Tìm theo tên sản phẩm: ${productName}`;
+        } else if (loaiValue === "type") {
+            const productType = document.getElementById("product-type").value;
+            loaiSanPham.innerHTML = `Tìm theo loại sản phẩm: ${productType}`;
+        }
+    }
+
+    const submitButton = document.getElementById("submit-search");
+    submitButton.addEventListener('click', submitSearch);
+});
+
